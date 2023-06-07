5fe73806154d194a72bfd0869068b6895cc42152
NatiG100 <natnaelgashu2022@gmail.com>
2023-06-07 16:35:35 +0300

    finish

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,51 @@
+import svg1 from './../../images/svg-1.svg';
+import svg3 from './../../images/svg-3.svg';
+import svg4 from './../../images/svg-4.svg';
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headLine: 'Login to your account at any time',
+    description: 'We have covered no matter where you are located. All you need is an internet connection and a phone or computer',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false,
+    img: svg1,
+}
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Access',
+    headLine: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    alt: 'stat',
+    dark: false,
+    primary: false,
+    darkText: true,
+    img: svg3,
+}
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join out Team',
+    headLine: 'Creating an account is extremely easy',
+    description: 'Get everything set up and ready in under 10 minutes. All you need to do is add your information and you\'re ready to go.',
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    alt: 'alt',
+    dark: false,
+    primary: false,
+    darkText: true,
+    img: svg4,
+}
